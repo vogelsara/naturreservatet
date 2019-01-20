121701dd923547dceef3a7737f956bdc7862bf25
Rudolf Ádám <sari.vogel.94@gmail.com>
2019-01-20 12:06:59 +0100

    Show the images of monkeys on results.php

--- results.php
+++ results.php
@@ -1,4 +1,13 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Document</title>
+</head>
+<body>
 
 There are/is <?php echo $_GET["numberOfMonkeys"]; ?> monkey(s)<br>
 There are/is <?php echo $_GET["numberOfGiraffes"]; ?> giraffe(s)<br>
@@ -11,7 +20,38 @@ abstract class Animal {
     abstract function makeSound();
 }
 
+class Monkey extends Animal {
+    function makeSound() {
+        echo "<script type='text/javascript'>alert('mak-mak');</script>";
+    }
+}
+
+class Tiger extends Animal {
+    function makeSound() {
+        echo "<script type='text/javascript'>alert('wáááááá');</script>";
+    }
+}
+
+class Giraffe extends Animal {
+    function makeSound() {
+        echo "<script type='text/javascript'>alert('i am a giraffe');</script>";
+    }
+}
+
+class Coco {
+
+}
+
 ?>
 
+<?php
 
+for ($i = 0; $i < $_GET["numberOfMonkeys"]; $i++) {
+    echo "<img src='img/monkey.png'/>\n";
+}
+
+?>
+
+</body>
+</html>
 
