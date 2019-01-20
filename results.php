d3818b5eab8710eacd3c368239124da7fc5566ef
Rudolf Ádám <sari.vogel.94@gmail.com>
2019-01-20 16:11:59 +0100

    Delete the section which shows the number of animals

--- results.php
+++ results.php
@@ -9,11 +9,6 @@
 </head>
 <body>
 
-There are/is <?php echo $_GET["numberOfMonkeys"]; ?> monkey(s)<br>
-There are/is <?php echo $_GET["numberOfGiraffes"]; ?> giraffe(s)<br>
-There are/is <?php echo $_GET["numberOfTigers"]; ?> tiger(s)<br>
-There are/is <?php echo $_GET["numberOfCocos"]; ?> coco(s)<br>
-
 <?php
 
 abstract class Animal {
